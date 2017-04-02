cf5972b691349af97936b3f8c9d23c6aaec964b1
cleverqin <705597001@qq.com>
2017-04-02 12:04:42 +0800

    修改页面样式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Headers></Headers>
-      <router-view class="view"></router-view>
+    <transition :name="'vux-pop-in'">
+      <router-view class="router-view"></router-view>
+    </transition>
     <Footers></Footers>
   </div>
 </template>
@@ -16,3 +18,77 @@
     }
   }
 </script>
+<style>
+  body{
+    overflow-x: hidden;
+  }
+  /**
+  * vue-router transition
+  */
+  .router-view {
+    width: 100%;
+    animation-duration: 0.5s;
+    animation-fill-mode: both;
+    backface-visibility: hidden;
+  }
+  .vux-pop-out-enter-active,
+  .vux-pop-out-leave-active,
+  .vux-pop-in-enter-active,
+  .vux-pop-in-leave-active {
+    will-change: transform;
+    height: 100%;
+    position: absolute;
+    left: 0;
+  }
+  .vux-pop-out-enter-active {
+    animation-name: popInLeft;
+  }
+  .vux-pop-out-leave-active {
+    animation-name: popOutRight;
+  }
+  .vux-pop-in-enter-active {
+    perspective: 1000;
+    animation-name: popInRight;
+  }
+  .vux-pop-in-leave-active {
+    animation-name: popOutLeft;
+  }
+  @keyframes popInLeft {
+    from {
+      opacity: 0;
+      transform: translate3d(-100%, 0, 0);
+    }
+    to {
+      opacity: 1;
+      transform: translate3d(0, 0, 0);
+    }
+  }
+  @keyframes popOutLeft {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+      transform: translate3d(-100%, 0, 0);
+    }
+  }
+  @keyframes popInRight {
+    from {
+      opacity: 0;
+      transform: translate3d(100%, 0, 0);
+    }
+    to {
+      opacity: 1;
+      transform: translate3d(0, 0, 0);
+    }
+  }
+  @keyframes popOutRight {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+      transform: translate3d(100%, 0, 0);
+    }
+  }
+</style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -5,17 +5,21 @@
       <input type="text" placeholder="搜索" v-model="keyWord">
     </div>
   </form>
-  <ul class="mui-table-view mui-table-view-chevron">
-    <li class="mui-table-view-cell mui-media" v-for="item in list">
-      <router-link :to="{ name: 'detail', params: { id: item.id }}" class="mui-navigate-right">
-        <img class="mui-media-object mui-pull-left" src="../assets/img/shuijiao.jpg">
-        <div class="mui-media-body">
-          {{item.name}}
-          <p class="mui-ellipsis">{{item.phone}}</p>
-        </div>
-      </router-link>
-    </li>
-  </ul>
+  <div class="contact-list-box">
+    <ul class="contact-list">
+      <li class="contact-item" v-for="item in list">
+        <router-link class="contact-item-link" :to="{ name: 'detail', params: { id: item.id }}">
+          <div class="contact-pic-box">
+            <img src="../assets/img/shuijiao.jpg" />
+          </div>
+          <div class="contact-msg-box">
+            <div class="contact-row-box"><span class="contact-name">{{item.name}}</span><span class="contact-tag">{{item.tag}}</span></div>
+            <div class="contact-row-box"><span class="contact-phone">{{item.phone}}</span></div>
+          </div>
+        </router-link>
+      </li>
+    </ul>
+  </div>
 </div>
 </template>
 <script>
@@ -44,5 +48,63 @@
     }
   }
 </script>
-
-
+<style >
+  .contact-list-box{
+    border-top: 1px solid #ddd;
+  }
+  .contact-item{
+    padding: 10px 15px;
+    background: #fff;
+    position: relative;
+  }
+  .contact-item:after{
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 15px;
+    height: 1px;
+    content: '';
+    -webkit-transform: scaleY(.5);
+    transform: scaleY(.5);
+    background-color: #919095;
+  }
+  .contact-item-link{
+    display: block;
+  }
+  .contact-item-link:after{
+    display: table;
+    content: "";
+    clear: both;
+  }
+  .contact-pic-box{
+    width: 60px;
+    height: 60px;
+    float: left;
+  }
+  .contact-pic-box img{
+    width: 60px;
+    height: 60px;
+    border-radius:50% ;
+  }
+  .contact-msg-box{
+    margin-left: 65px;
+    padding: 5px;
+  }
+  .contact-row-box{
+    line-height: 25px;
+  }
+  .contact-name{
+    font-size: 16px;
+    color: #000000;
+    margin-right: 15px;
+    font-weight: bold;
+  }
+  .contact-tag{
+    font-size: 12px;
+    color: #999;
+  }
+  .contact-phone{
+    font-size: 14px;
+    color: #666;
+  }
+</style>
